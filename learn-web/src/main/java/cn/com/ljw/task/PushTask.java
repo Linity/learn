996f635764c268f722dcad5e35ea3f55adc4c54c
Steph_Lin <1784684258@qq.com>
2020-11-20 16:55:00 +0800

    websocket实现定时推送

---
+++ learn-web/src/main/java/cn/com/ljw/task/PushTask.java
@@ -0,0 +1,8 @@
+package cn.com.ljw.task;
+
+/**
+ * @author Steph_Lin
+ * @date 2020/11/20
+ */
+public class PushTask {
+}
---
+++ learn-web/src/main/java/cn/com/ljw/websocket/PushMessageEncoder.java
@@ -0,0 +1,8 @@
+package cn.com.ljw.websocket;
+
+/**
+ * @author Steph_Lin
+ * @date 2020/11/20
+ */
+public class PushMessageEncoder {
+}
